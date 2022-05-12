043205de6a13616674cf18c9c2cfd259fa3bf11d
sunwukong73 <34038392+sunwukong73@users.noreply.github.com>
2022-05-12 10:49:56 +0100

    Update data.py

--- stats/data.py
+++ stats/data.py
@@ -22,5 +22,5 @@ identifiers=identifiers.fillna(method='ffill')
 identifiers.columns=['game_id','year']
 games=pd.concat([games, identifiers], axis=1, sort=False)
 games=games.fillna(' ')
-games.loc[:,'type'] = pd.Categorical(games.loc[:,'type',], categories =['info','start','play','com','sub','data'])
+games.loc[:,'type'] = pd.Categorical(games.loc[:,'type',])
 print(games.head())
